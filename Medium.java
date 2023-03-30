a3aa7842b23ceaf2748b3eca650497dd38ea3231
Shachi Singhal <122254334+Shachi1505@users.noreply.github.com>
2023-03-31 00:00:03 +0530

    Update Medium.java
    Given an array of integers, print a sum triangle from it such that the first level(the bottom level in triangular fashion) has all array elements. From then, at each level, the number of elements is one less than the previous level and elements at the level is the sum of consecutive two elements in the previous level.

--- Medium.java
+++ Medium.java
@@ -10,7 +10,13 @@ int[] b = new int[a.length-1]; // creating new array
 for(int i=0; i<a.length-1; i++){
     b[i] = a[i] + a[i+1]; // recursion call
 }
-tri(b);
+tri(b); // To print new array 
     }
-    
 }
+/* Output
+[48]
+[20, 28]
+[8, 12, 16]
+[3, 5, 7, 9]
+[1, 2, 3, 4, 5]
+*/
